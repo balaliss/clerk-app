36077fd1c20b4e7c1b47161b9970e3758ece98f0
balaliss <balalis43@gmail.com>
2023-11-14 10:09:10 -0500

    updated page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,10 @@
-export default function Home () {
-  return(
+export default function Home() {
+  return (
     <>
-    <h1>Home</h1>
+      <h1 className='text-2xl font-bold mb-5'>Welcome</h1>
+      <p className='mb-5'>
+        Steve Balalis created this demo website with the help of Traversy Media. I used Clerk for authentication and nextjs. Thanks for stopping by! Cheers!
+      </p>
     </>
-  )
+  );
 }
